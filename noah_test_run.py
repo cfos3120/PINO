6323b16dc6cad5b7149e4dca8491219bfaea4ecb
cfos3120 <cfos3120@uni.sydney.edu>
2022-08-09 18:08:37 +1000

    updated libaries and print message when completed

--- noah_test_run.py
+++ noah_test_run.py
@@ -1,9 +1,16 @@
 print('Hello World')
 
-from argparse import ArgumentParser
 import yaml
-
+from argparse import ArgumentParser
+import math
 import torch
+from torch.utils.data import DataLoader
 
-from tqdm import tqdm
-from timeit import default_timer
+from solver.random_fields import GaussianRF
+from train_utils import Adam
+from train_utils.datasets import NSLoader, online_loader, DarcyFlow
+from train_utils.train_3d import mixed_train
+from train_utils.train_2d import train_2d_operator
+from models import FNN3d, FNN2d
+
+print('Finished Importing Libaries')
