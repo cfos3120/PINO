803d75c5f872aaf7b3384e90725ff409de026a4e
cfos3120 <cfos3120@uni.sydney.edu>
2022-11-08 10:10:44 +1100

    added velocity conversion of dataset

--- noah_test_run.py
+++ noah_test_run.py
@@ -4,6 +4,8 @@ import yaml
 from argparse import ArgumentParser
 import math
 import torch
+import numpy as np
+from train_utils.utils import vor2vel
 #from torch.utils.data import DataLoader
 
 #from solver.random_fields import GaussianRF
@@ -16,20 +18,16 @@ import torch
 print('Finished Importing Libaries')
 
 if __name__ == '__main__':
-    # parse options
-    parser = ArgumentParser(description='Basic paser')
-    parser.add_argument('--config_path', type=str, help='Path to the configuration file')
-    parser.add_argument('--log', action='store_true', help='Turn on the wandb')
-    args = parser.parse_args()
-
-    print('Finished Parsing')
-
-    config_file = args.config_path
-    with open(config_file, 'r') as stream:
-        config = yaml.load(stream, yaml.FullLoader)
-
-    print('Finished Configuration')
-
-    t = torch.rand(10, 10)
-    rfftn = torch.fft.rfftn(t)
-    print(rfftn.size())
+    try: data = np.load(r"C:\Users\Noahc\Documents\USYD\PHD\8 - Github\PINO datasets\NS_fft_Re500_T4000.npy" ) 
+    except: 
+        try: data = np.load(r'/project/MLFluids/NS_fft_Re500_T4000.npy')
+        except: raise('No Dataset File Found')
+    else: print('Dataset Allocated')
+    
+    print('Input Shape: ', data.shape)
+    data = torch.tensor(data).permute(0,2,3,1)
+    sol_cartesian = np.zeros((data.shape[0], data.shape[1], data.shape[2], data.shape[3], 2))
+    sol_cartesian[: , : , : , :, 0], sol_cartesian[: , : , : , :, 1] = vor2vel(data)
+    sol_cartesian = torch.tensor(sol_cartesian).permute(0,3,1,2,4)
+    print('Output Shape: ', sol_cartesian.shape)
+    print('Conversion Complete')
