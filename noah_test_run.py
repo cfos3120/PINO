aef8ef283f143e5a9ef2a077d095c8271c90ca6f
cfos3120 <cfos3120@uni.sydney.edu>
2022-11-08 18:06:12 +1100

    fix

--- noah_test_run.py
+++ noah_test_run.py
@@ -5,15 +5,8 @@ from argparse import ArgumentParser
 import math
 import torch
 import numpy as np
-from train_utils.utils import vor2vel
+from train_utils import utils
 #from torch.utils.data import DataLoader
-
-#from solver.random_fields import GaussianRF
-#from train_utils import Adam
-#from train_utils.datasets import NSLoader, online_loader, DarcyFlow
-#from train_utils.train_3d import mixed_train
-#from train_utils.train_2d import train_2d_operator
-#from models import FNN3d, FNN2d
 
 print('Finished Importing Libaries')
 
@@ -27,7 +20,7 @@ if __name__ == '__main__':
     print('Input Shape: ', data.shape)
     data = torch.tensor(data).permute(0,2,3,1)
     sol_cartesian = np.zeros((data.shape[0], data.shape[1], data.shape[2], data.shape[3], 2))
-    sol_cartesian[: , : , : , :, 0], sol_cartesian[: , : , : , :, 1] = vor2vel(data)
+    sol_cartesian[: , : , : , :, 0], sol_cartesian[: , : , : , :, 1] = utils.vor2vel(data)
     sol_cartesian = torch.tensor(sol_cartesian).permute(0,3,1,2,4)
     print('Output Shape: ', sol_cartesian.shape)
 
